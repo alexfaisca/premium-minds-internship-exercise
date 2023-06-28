148ddfb3aceb901dad549c885842bf1b7cecf1ee
ist1100120 <alexandre.f.coelho@tecnico.ulisboa.pt>
2023-06-28 07:38:03 +0100

    first iteration comments fix

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -16,7 +16,7 @@ class SnailShellPattern implements ISnailShellPattern {
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
     return executor.submit(() -> {
-      // Traverse parameters
+      // Matrix traverse parameters
       int left = 0, top = 0, right = matrix.length, bottom = matrix.length, horizontal_increment = 1, vertical_increment = 1;
       // Shell parameters
       int[] snail_shell = new int[matrix.length * matrix.length];
@@ -25,35 +25,42 @@ class SnailShellPattern implements ISnailShellPattern {
       while(shell_piece < matrix.length * matrix.length) {
         // Add shell piece
         snail_shell[shell_piece++] = matrix[row][column];
+
         // Update traverse parameters
-        if (horizontal_increment == vertical_increment) { // Check if movement is horizontal
-          if(horizontal_increment > 0) { // Check movement direction
+        if (horizontal_increment == vertical_increment) { // Case movement is horizontal
+
+          if(horizontal_increment > 0) {  // Case movement is left to right
             if (column < right - 1) { // Check if right boundary reached
               column += horizontal_increment;
               continue;
             }
-            top++; // Top horizontal completed: update top boundary
-          } else if (column > left) { // Check if left boundary reached
+            top++; // Right boundary reached: constrict top boundary
+          } else if (column > left) { // Check if left boundary reached (movement is right to left)
             column += horizontal_increment;
             continue;
-          } else bottom--; // Bottom horizontal completed: update bottom boundary
+          } else bottom--; // Left boundary reached: constrict bottom boundary
+
           // Change movement direction
           horizontal_increment = -horizontal_increment;
           row += vertical_increment;
-        } else {
-          if (vertical_increment > 0) { // Check movement direction
+
+        } else { // Case movement is vertical
+
+          if (vertical_increment > 0) { // Case movement is top to bottom
             if (row < bottom - 1) { // Check if bottom boundary reached
               row += vertical_increment;
               continue;
             }
-            right--; // Right vertical completed: update right boundary
-          } else if (row > top) { // Check if top boundary reached
+            right--; // Bottom boundary reached: constrict right boundary
+          } else if (row > top) { // Check if top boundary reached (movement is bottom to top)
             row += vertical_increment;
             continue;
-          } else left++; // Left vertical completed: update left boundary
+          } else left++; // Top boundary reached: constrict left boundary
+
           // Change movement direction
           vertical_increment = -vertical_increment;
           column += horizontal_increment;
+
         }
     }
 
